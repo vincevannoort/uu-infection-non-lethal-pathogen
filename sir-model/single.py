02c81497ab0a1e857e61d5bdd4baf72eac2c9466
Vince van Noort <vincevannoort@gmail.com>
2020-04-06 19:14:02 +0200

    update sir single run

--- sir-model/single.py
+++ sir-model/single.py
@@ -1,27 +1,22 @@
 from model import SIRModel
 import matplotlib.pyplot as plt
+from matplotlib import colors
 import itertools
 import numpy as np
 
 color_dict = {
-    'Susceptible': '#eeeeee', 
+    'Susceptible': '#eeeeee',
     'Infected': '#ff5e5e',
     'Recovered': '#d6ff37'
 }
 
-color_dict_states = {
-    '1': '#eeeeee',
-    '2': '#ff5e5e',
-    '3': '#d6ff37'
-}
-
 # Configuration
-duration = 120
-visualise_each_x_timesteps = -1
-grid_size = 150
+duration = 2000
+visualise_each_x_timesteps = 100
+grid_size = 200
 
-model = SIRModel(grid_size, grid_size, infectivity=4.2, infection_duration=70, immunity_duration=100,
-                 mutation_probability=0.01, mutation_strength=1, visualise_each_x_timesteps=visualise_each_x_timesteps)
+model = SIRModel(grid_size, grid_size, infectivity=4.2, infection_duration=70, immunity_duration=70,
+                 mutation_probability=0.1, mutation_strength=5, visualise_each_x_timesteps=visualise_each_x_timesteps)
 
 for i in range(duration):
     model.step()
@@ -35,15 +30,18 @@ title = f'Infection duration: {model.infection_duration}, Immunity duration: {mo
 # plot amount of susceptible, recoveries and infected
 data.plot.area(color=color, title=title).legend(loc='upper right')
 plt.margins(0)
-plt.savefig(f'figures/model-result--single--inf-{model.infection_duration}--imm-{model.immunity_duration}.pdf')
+plt.savefig(
+    f'figures/model-result--single--inf-{model.infection_duration}--imm-{model.immunity_duration}.pdf')
 
 # plot infection duration
 data_mid.plot()
 plt.savefig(f'figures/inf-dur-result.pdf')
 
+
+cmap = colors.ListedColormap(['#eeeeee', '#ff5e5e', '#d6ff37'])
 for index, grid in enumerate(model.grids_saved):
     grid = np.reshape(grid, (-1, grid_size))
     plt.figure()
-    plt.imshow(grid)
+    plt.imshow(grid, cmap=cmap)
     plt.savefig(f'figures/grids/grid--{index}.pdf')
     plt.close()
