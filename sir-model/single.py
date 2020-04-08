2da094b8e04442dc59ff4ab30206efdf3ee7c65b
Vince van Noort <vincevannoort@gmail.com>
2020-04-08 12:53:54 +0200

    adjustments

--- sir-model/single.py
+++ sir-model/single.py
@@ -8,10 +8,11 @@ import numpy as np
 from matplotlib.backends.backend_pdf import PdfPages
 
 cmap = colors.ListedColormap(['#eeeeee', '#ff5e5e', '#d6ff37'])
-color_dict = { 'Susceptible': '#eeeeee', 'Infected': '#ff5e5e', 'Recovered': '#d6ff37' }
+color_dict = {'Susceptible': '#eeeeee',
+              'Infected': '#ff5e5e', 'Recovered': '#d6ff37'}
 
 # Configuration
-duration = 125 * 100
+duration = 125 * 1000
 visualise_each_x_timesteps = 125
 grid_size = 120
 
@@ -22,8 +23,10 @@ model = SIRModel(
     infection_duration=70,
     immunity_duration=130,
     mutation_probability=0.1,
-    mutation_strength= 1,
+    mutation_strength=1,
     visualise_each_x_timesteps=visualise_each_x_timesteps)
+
+start_time = time.strftime("%Y%m%d-%H%M%S")
 
 for i in range(duration):
     model.step()
@@ -34,31 +37,30 @@ data_mid = model.datacollector_meaninfectionduration.get_model_vars_dataframe()
 color = [color_dict.get(x, '#333333') for x in data.columns]
 title = f'Infection duration: {model.infection_duration}, Immunity duration: {model.immunity_duration}'
 
-# plot amount of susceptible, recoveries and infected
+# plot: amount of susceptible, recoveries and infected
 data.plot.area(color=color, title=title).legend(loc='upper right')
 plt.margins(0)
-plt.savefig(
-    f'figures/model-result--single--inf-{model.infection_duration}--imm-{model.immunity_duration}.pdf')
+plt.savefig(f'figures/{start_time}-graph.pdf')
 
-# plot infection duration
+# plot: infection duration
 data_mid.plot()
-plt.savefig(f'figures/inf-dur-result.pdf')
+plt.savefig(f'figures/{start_time}-infection.pdf')
 
-multipage = PdfPages(f'figures/grid-{grid_size}-{duration}-{visualise_each_x_timesteps}-{time.strftime("%Y%m%d-%H%M%S")}.pdf')
-
-# print(math.ceil((duration / visualise_each_x_timesteps) / 25))
-# print(np.array_split(np.arange(35.0), 2))
+# plot: grid
+multipage = PdfPages(f'figures/{start_time}-grids.pdf')
 
 for index_grids, grids in enumerate(np.array_split(model.grids_saved, math.ceil((duration / visualise_each_x_timesteps) / 25))):
     fig, axes = plt.subplots(nrows=5, ncols=5, figsize=(15, 15))
     plt.subplots_adjust(wspace=0.3, hspace=0.3)
 
-    fig.suptitle(f'Infectivity: {model.infectivity} \nInfection duration: {model.infection_duration} \nImmunity duration: {model.immunity_duration}', fontsize=24)
+    fig.suptitle(
+        f'Infectivity: {model.infectivity} \nInfection duration: {model.infection_duration} \nImmunity duration: {model.immunity_duration}', fontsize=24)
     for index_axes, ax in enumerate(axes.reshape(-1)):
         try:
             grid = grids[index_axes]
             grid = np.reshape(grid, (-1, grid_size))
-            ax.set_title(f'iteration: {index_grids * 25 * visualise_each_x_timesteps + index_axes * 25}')
+            ax.set_title(
+                f'iteration: {index_grids * visualise_each_x_timesteps * 25 + index_axes * visualise_each_x_timesteps}')
             ax.imshow(grid, cmap=cmap, interpolation='none')
         except IndexError:
             break
@@ -66,4 +68,3 @@ for index_grids, grids in enumerate(np.array_split(model.grids_saved, math.ceil(
     multipage.savefig()
 
 multipage.close()
-
